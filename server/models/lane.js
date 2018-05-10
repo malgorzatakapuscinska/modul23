269a2fb3e3e496ad96266d5319284dcba9787c53
Małgorzata Kapuścińska <m_kapuscinska1@o2.pl>
2018-05-10 12:53:54 +0200

    Create schema and model for Lane and Note

--- server/models/lane.js
+++ server/models/lane.js
@@ -1,8 +1,15 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
+mongoose.plugin(schema => { schema.options.usePushEach = true });
+
+// create schema for lane
 
 const laneSchema = new Schema({
-
+  name: {type: 'String', required: true },
+  notes: [{type: Schema.ObjectId, ref: 'Note', required: true}],
+  id: {type: 'String', required: true, unique: true},
 });
+
+// create and export model for Lane
 
 export default mongoose.model('Lane', laneSchema);
--- server/models/note.js
+++ server/models/note.js
@@ -1,8 +1,13 @@
 import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
-const noteSchema = new Schema({
+//create schema for note
 
+const noteSchema = new Schema({
+  task: {type: 'String', required: true},
+  id: {type: 'String', required: true}
 });
+
+//create and export model for note
 
 export default mongoose.model('Note', noteSchema);
